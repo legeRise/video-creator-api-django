7c089d2a10ec5462186e4d1648b63e535556dc86
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-16 12:31:01 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -3,8 +3,9 @@ venvname="env92"
 if [ ! -d "$venvname" ]; then
     python -m venv "$venvname"
 fi
-pip install -r requirements.txt
+
 source "$venvname/bin/activate"
+pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 gunicorn -w 4 -b 0.0.0.0:9200 --reload myproj.wsgi:application
