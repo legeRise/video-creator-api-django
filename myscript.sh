3fc49757e285e6b8e0f41ec84161f57503d2a993
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-16 12:10:07 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,12 +2,10 @@ venvname="env92"
 
 if [ ! -d "$venvname" ]; then
     python -m venv "$venvname"
+    pip install -r requirements.txt
 fi
 
 source "$venvname/bin/activate"
-pip install -r requirements.txt
-
-echo "this is working dir92 : $PWD"
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:9200
