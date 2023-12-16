be97a117b24edcff3256c3270d8ba23806ee4191
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-16 11:59:06 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -7,6 +7,8 @@ fi
 source "$venvname/bin/activate"
 pip install -r requirements.txt
 
+import os
+print(os.getcwd())
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:9200
