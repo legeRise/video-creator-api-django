eab4bbfb727231447e982092f4b8a1160ea7dc5d
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-16 13:45:31 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -6,6 +6,7 @@ fi
 
 source "$venvname/bin/activate"
 pip install -r requirements.txt
+python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
 gunicorn -w 4 -b 0.0.0.0:9200 --reload myproj.wsgi:application
