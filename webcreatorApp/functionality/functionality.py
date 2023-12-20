918cafaa65597845f4d3d274ec5af629af9f7790
legeRise <139732519+legeRise@users.noreply.github.com>
2023-12-20 18:56:42 +0500

    Update functionality.py

--- webcreatorApp/functionality/functionality.py
+++ webcreatorApp/functionality/functionality.py
@@ -354,7 +354,6 @@ class videoFunctions:
         # step 1: Specify the input video path,output video path and Define the text properties
         media=settings.MEDIA_ROOT
         user= os.path.join(media,f'user_{id}')
-        print('titlebar: 42342',titlebar,media)
         if titlebar:
             vf = videoFunctions()
             vf.titleBar(id=id)  # after calling this function title is added and the  video is saved as 'title_added.mp4'
